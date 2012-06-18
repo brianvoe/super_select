bb11ebfceb8085b3f2346d6c0a9b20cefe345f2c
Brian Voelker <brian@silverlinecreative.com>
2012-06-18 18:03:35 -0500

    Work on select buildout.

--- superselect.js
+++ superselect.js
@@ -25,25 +25,40 @@
               orig_id = Math.ceil(Math.random() * 100000);
               orig_select.attr('id', orig_id);
             }
-            alert(orig_select.attr('id'));
-            var new_select = $('<div></div>');
+            var new_id = orig_id+'_supsel';
+            var new_li_num = 0;
+            var new_select = '<div id="'+new_id+'_select'+'" class="supsel_select"></div>';
+            new_select += '<div id="'+new_id+'_div'+'" class="supsel_div">';
+            new_select += '<div class="supsel_search"></div>';
+            new_select += '<div class="supsel_results"><ul></ul></div>';
+            new_select += '</div>';
+            new_select = $(new_select);
+            var new_array = new Array();
+            var new_results = '';
 
             /* Replace default options with requested options */
             info.options = $.extend({}, select_options, options);
 
-            /* If  */
-
             /* Hide select dropdown */
-            orig_select.hide();
+            //orig_select.hide();
 
             /* Create new dropdown */
             new_select.insertAfter(orig_select);
 
             /* Append values from original select */
-            $("#selectId > option").each(function() {
-              alert(this.text + ' ' + this.value);
+            $('#'+orig_id+' > option').each(function() {
+              new_results += '<li id="'+new_id+'_'+new_li_num+'">'+this.text+'</li>';
+              new_array[new_id+'_'+new_li_num] = this.value;
+              new_li_num++;
             });
-      
+            $('#'+new_id+'_div .supsel_results ul').append(new_results);
+
+            /* Add click events to results li */
+            $('#'+new_id+'_div .supsel_results ul li').click(function() {
+              /* Set original select dropdown */
+              $('#'+orig_id).val(new_array[$(this).attr('id')]);
+            });
+
         },
         destroy: function() {
             var info = $(this).data('superselect');
@@ -58,7 +73,7 @@
         return this.each(function() {
             /* Only allow select dropdown */
             if($(this).is('select')) {
-                // Method calling logic
+                /* Method calling logic */
                 if (super_select_funcs[options]) {
                   if($(this).data('superselect')) {
                     super_select_funcs[options].apply(this);
